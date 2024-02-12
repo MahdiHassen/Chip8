fa70d55477de2345f028cbd7ec582a338d6551b0
Mahdi <110603934+MahdiHassen@users.noreply.github.com>
2024-02-12 01:22:39 -0500

    added more opcodes

--- main.c
+++ main.c
@@ -11,13 +11,13 @@
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-uint8_t graphics[SCREEN_HEIGHT][SCREEN_WIDTH] = {1}; // 64x32 pixels on the display, initialized with 0's but with 1 for test
+uint8_t graphics[SCREEN_HEIGHT][SCREEN_WIDTH] = {0}; // 64x32 pixels on the display, initialized with 0's but with 1 for test
 
 uint16_t pc; // Program counter
 uint16_t I; // Index register
-uint16_t sp; // Stack pointer
 uint8_t V[16]; // 16 general purpose registers
-uint16_t stack[16]; // Stack
+uint16_t sp=0; // Stack pointer
+uint16_t stack[16] = {0}; // Stack
 uint8_t memory[0x1000]; // 4KB memory
 
 uint16_t opcode=0; // Current opcode
@@ -75,6 +75,7 @@ void loadROM(char *romName){ //loads rom at 0x200
 void fetchOpcode() { //fetches opcode from memory, increments pc by 2
     opcode = memory[pc] << 8 | memory[pc + 1];
     pc += 2;
+    printf("Fetched opcode: 0x%X\n", opcode);
 }
 
 void executeCycle() {
@@ -83,14 +84,26 @@ void executeCycle() {
     switch (opcode & 0xF000) {
         case 0x0000:
             // Handle opcodes starting with 0x0
-            case 0x00E0:{ // Clear the display
+
+        switch (opcode & 0x00FF) {
+
+            case 0x00E0:{ // Clear the display (0x00E0)
                 for(int i = 0; i < SCREEN_HEIGHT; i++) {
                     for(int j = 0; j < SCREEN_WIDTH; j++) {
                         graphics[i][j] = 0;
-                    }
-                }
+                            }
+                        }
+                        
+                    }break;
+
+            case 0x00EE: { // Return from a subroutine (pop the stack, )
+
+
+                pc = stack[sp]; // Set pc to the top of the stack
+                sp--; // Decrement stack pointer
             }
                 break;
+        }
             break;
 
         case 0x1000:{
@@ -99,17 +112,43 @@ void executeCycle() {
             }
             break;
 
-        case 0x2000:
-            // Handle opcodes starting with 0x2
+        case 0x2000: {
+            // Handle opcodes starting with 0x2 (Call subroutine at NNN, 0x2NNN, pushes pc to stack)
+
+            printf("pc: %d\n", pc);
+            printf("sp is: %d\n", sp);
+            printf("stack[sp], before: %d\n", stack[sp]);
+            stack[sp] = pc; // Store current pc on the stack
+            printf("stack[sp], set: %d\n", stack[sp]);
+            sp++; // Increment stack pointer
+            printf("sp is now: %d\n", sp);
+            printf("pc going to be set to: %d\n", opcode & 0x0FFF);
+            pc = opcode & 0x0FFF; // Set pc to NNN
+            }
+
             break;
-        case 0x3000:
-            // Handle opcodes starting with 0x3
+        case 0x3000:{
+            // Handle opcodes starting with 0x3, (Skip next instruction if Vx == NN, 0x3XNN, increments pc by 2)
+            if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF)) {
+                pc += 2;
+            }
+            }        
             break;
-        case 0x4000:
-            // Handle opcodes starting with 0x4
+
+        case 0x4000:{
+            // Handle opcodes starting with 0x4 (Skip next instruction if Vx != NN, 0x4XNN, increments pc by 2)
+            if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF)) {
+                pc += 2;
+            }
+            }
+
             break;
-        case 0x5000:
-            // Handle opcodes starting with 0x5
+        case 0x5000:{
+            // Handle opcodes starting with 0x5 (Skip next instruction if Vx == Vy, 0x5XY0, increments pc by 2)
+            if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4]) {
+                pc += 2;
+            }
+            }
             break;
 
         case 0x6000:{
@@ -124,11 +163,90 @@ void executeCycle() {
         }
             break;
 
-        case 0x8000:
+        case 0x8000:{
             // Handle opcodes starting with 0x8
+            switch (opcode & 0x800F) {
+
+            case(0x8000):{
+                // Set Vx to Vy 0x8XY0
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];
+                break;
+            }    
+        
+            case(0x8001):{
+                // Set Vx to Vx | Vy 0x8XY1
+                V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];
+                break;
+            }
+
+            case(0x8002):{
+                // Set Vx to Vx & Vy 0x8XY2
+                V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];
+                break;
+            }
+
+            case(0x8003):{
+                // Set Vx to Vx xor Vy 0x8XY3
+                V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+                break;
+            }
+
+            case(0x8004):{
+                // Add Vy to Vx, set VF to 1 if there is a carry 0x8XY4
+                if (V[(opcode & 0x00F0) >> 4] > (0xFF - V[(opcode & 0x0F00) >> 8])) {
+                    V[0xF] = 1; // Set VF to 1
+                } else {
+                    V[0xF] = 0; // Set VF to 0
+                }
+                V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4];
+                break;
+            }
+
+            case(0x8005):{
+                // Subtract Vy from Vx, set VF to 0 if there is a borrow 0x8XY5
+                if (V[(opcode & 0x00F0) >> 4] > V[(opcode & 0x0F00) >> 8]) {
+                    V[0xF] = 0; // Set VF to 0
+                } else {
+                    V[0xF] = 1; // Set VF to 1
+                }
+                V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4];
+                break;
+            }
+
+            case(0x8006):{
+                // Store the least significant bit of Vx in VF and then shift Vx to the right by 1 0x8XY6
+                V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x1; // Store the least significant bit of Vx in VF
+                V[(opcode & 0x0F00) >> 8] >>= 1; // Shift Vx to the right by 1
+                break;
+            }
+
+            case(0x8007):{
+                // Set Vx to Vy - Vx, set VF to 0 if there is a borrow 0x8XY7
+                if (V[(opcode & 0x0F00) >> 8] > V[(opcode & 0x00F0) >> 4]) {
+                    V[0xF] = 0; // Set VF to 0
+                } else {
+                    V[0xF] = 1; // Set VF to 1
+                }
+                V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4] - V[(opcode & 0x0F00) >> 8];
+                break;
+            }
+
+            case(0x800E):{
+                // Store the most significant bit of Vx in VF and then shift Vx to the left by 1 0x8XYE
+                V[0xF] = V[(opcode & 0x0F00) >> 8] >> 7; // Store the most significant bit of Vx in VF
+                V[(opcode & 0x0F00) >> 8] <<= 1; // Shift Vx to the left by 1
+                break;
+            }
+
+        }
+    }
             break;
-        case 0x9000:
-            // Handle opcodes starting with 0x9
+        case 0x9000:{
+            // Handle opcodes starting with 0x9 (Skip next instruction if Vx != Vy 0x9XY0, increments pc by 2)
+            if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4]) {
+                pc += 2;
+            }
+            }
             break;
 
         case 0xA000:{
@@ -137,11 +255,19 @@ void executeCycle() {
             }
             break;
 
-        case 0xB000:
-            // Handle opcodes starting with 0xB
+        case 0xB000:{
+            // Handle opcodes starting with 0xB (Jump to address NNN + V0 0xBNNN)
+            pc = (opcode & 0x0FFF) + V[0]; // Set pc to NNN + V0
+            }
             break;
-        case 0xC000:
-            // Handle opcodes starting with 0xC
+
+        case 0xC000:{
+            // Handle opcodes starting with 0xC, (Random number AND NN, 0xCXNN, sets Vx to random number AND NN)
+            
+            uint8_t random = rand() % 0xFF; // Generate a random number between 0 and 255
+            V[(opcode & 0x0F00) >> 8] = random & (opcode & 0x00FF); // Set Vx to random & NN
+            }
+
             break;
 
         case 0xD000:{
@@ -173,9 +299,9 @@ void executeCycle() {
         case 0xF000:
             // Handle opcodes starting with 0xF
             break;
+
         default:
-            printf("Error, Opcode isnt defined");
-            break;
+            printf("Unknown opcode: 0x%X\n", opcode);
     }
 }
 
@@ -226,7 +352,12 @@ void displayGraphics() {
     SDL_RenderPresent(renderer);
 }
 
+void getInput() {
 
+    
+
+
+}
 
 
 int main(int argc, char **argv) {
@@ -237,7 +368,7 @@ int main(int argc, char **argv) {
 //TODO: Add sound
 
     loadFont();
-    loadROM("IBM Logo.ch8");
+    loadROM("test_opcode.ch8");
     setupGraphics();
 
     SDL_Event event;
@@ -263,9 +394,11 @@ int main(int argc, char **argv) {
             drawGraphics(); 
             displayGraphics();
 
+            printf("sp (post exec) is: %d\n", sp);
+
             lastCycleTime = currentTime;
         } else {
-            SDL_Delay(2); 
+            SDL_Delay(1); 
         } 
 }
         
